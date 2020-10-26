3ac1b5d34cb71e118e192ced7aa5e0a07c2cbcac
jai sai chand <t.jaisaichand4@gmail.com>
2020-10-26 16:29:38 +0530

    ...

---
+++ api/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+router.post('/signup',(req,res,next)=>{
+    
+})
+
+module.exports = router;
